de8611cd4a3662e0e3d7b849381e01a7a2a15407
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-11-15 11:45:17 -0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles.css';
+import faqs from './questArray.js';
+
+export default function App() {
+  return (
+    <div>
+      <Questions />
+    </div>
+  );
+}
+
+function Questions () {
+  return (
+    <div>
+      Questions
+    </div>
+  )
+}
