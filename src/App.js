766c8750a5d164921ce2a344675a70605d966fde
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-11-16 11:06:23 -0500

    Conditional rendering for open/close questions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './styles.css';
 // import faqs from './questArray.js';
 
@@ -38,12 +39,14 @@ function Questions({data}) {
 }
 
 function IndivQuestion({num, title, text}) {
+  const[isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='item'>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon" >{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
